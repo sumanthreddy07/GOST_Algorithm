87a3fef42c9e9234e9ef803d85990ba5da508991
sumanthreddy07 <kota.sumanthreddy@gmail.com>
2021-03-04 18:35:07 +0530

    cleared issue

--- encryption.py
+++ encryption.py
@@ -1,5 +1,6 @@
+import os
 from S_BOX import sbox_fun
-from KEY import make_key
+from key import make_key
 from decryption import decrypt
 def leftshift11(Y):
     Y = Y[11:]+Y[:11]
@@ -8,71 +9,57 @@ def leftshift11(Y):
 def encrypt_32(val,k):
 
     Li,Ri = val[:32], val[32:]
-    #Ri = Ri[::-1]
-    #Li = Li[::-1]
+    Ri = Ri[::-1]
+    Li = Li[::-1]
 
-    for i in range(1):
+    for i in range(24):
         X = int(Ri,2) + int(k[i%8],2)                       # integer
         X = X%pow(2,32)
         X = format(X,'032b')                                # binary string
-        Y = sbox_fun(str(X))                                # string with f(x)(1-8) = sbox(x)(1-8)
-        Y = leftshift11(Y)
+        X = sbox_fun(str(X))                                # string with f(x)(1-8) = sbox(x)(1-8)
+        X = leftshift11(X)
         
-        Li,Ri = Ri,str(format(int(Y,2)^int(Li,2),'032b'))
-
-    """
-    for i in range(24):
-        X = int(Ri,2) + int(k[i%8],2)
-        X = X%pow(2,32)
-        X = format(X,'032b')
-        Y = sbox_fun(str(X))
-        Y = leftshift11(Y)
-        
-        Li = Ri
-        Ri = str(format(int(Y,2)^int(Li,2),'032b'))
-
+        Li,Ri = Ri,str(format(int(X,2)^int(Li,2),'032b'))
+    
     for i in range(8):
-        X = int(Ri,2) + int(k[7-i%8],2)
+        X = int(Ri,2) + int(k[7-i],2)                       # integer
         X = X%pow(2,32)
-        X = format(X,'032b')
-        Y = sbox_fun(str(X))
-        Y = leftshift11(Y)
-                
-        Li = Ri
-        Ri = str(format(int(Y,2)^int(Li,2),'032b'))
-    """
+        X = format(X,'032b')                                # binary string
+        X = sbox_fun(str(X))                                # string with f(x)(1-8) = sbox(x)(1-8)
+        X = leftshift11(X)
+        
+        Li,Ri = Ri,str(format(int(X,2)^int(Li,2),'032b'))
+
     return Li,Ri
 
 
 def encrypt():
-
     """
-    print("reading file...")
-    with open('inputfile.txt') as f:
-        lines = f.readlines()
+    __location__ = os.path.realpath(
+    os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
-    for line in lines:
-        if len(line) > 8:
+    with open(os.path.join(__location__, 'original.txt'),'r') as f:
+        lines = f.read()
 
-    block_size = 8
+    print(lines)
+    print(type(lines))
+
+    binstring = ''.join(format(ord(i), '08b') for i in lines)
+    print(binstring,type(binstring))
     """
-
     text = "abcdefgh"
 
     #key = input("Enter 32 Character Key: ")
     key = "12345678123456781234567812345678"                                        #key is string-number
-    #text = "00000000"
-    #key = "0000000000000000000000000000000000000000000000000000000000000000"
     k = []
     k = make_key(key)                                                               #returns a list of 8 keys each of size 32 bits
     val = ""
     for x in text:
         val = val + format(ord(x),'08b')                                            #convert text to binary string
-
+    print(val[:32]," ",val[32:])
     Li,Ri = encrypt_32(val,k)
 
-    #val = Ri[::-1] + Li[::-1]
-    val = Li + Ri
+    val = Li[::-1] + Ri[::-1]
     A = []
     for j in range(0, 64, 8):
         A.append( val[j:j+8] )                                                      #convert to binary chars
@@ -81,7 +68,6 @@ def encrypt():
         A[i] = chr(int(A[i],2))                                                     #convert to char
 
     print(A)
-    print(val)
 
     print("would you like to decrypt? 1:Yes 2:Exit")
     choice = input()
