bc1e7e85143932e31b2176bfc5ba9733289d6858
sumanthreddy07 <kota.sumanthreddy@gmail.com>
2021-03-04 23:05:22 +0530

    encrypt txt file

--- encryption.py
+++ encryption.py
@@ -34,44 +34,37 @@ def encrypt_32(val,k):
 
 
 def encrypt():
-    """
-    __location__ = os.path.realpath(
-    os.path.join(os.getcwd(), os.path.dirname(__file__)))
+    __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
     with open(os.path.join(__location__, 'original.txt'),'r') as f:
         lines = f.read()
 
     print(lines)
-    print(type(lines))
-
     binstring = ''.join(format(ord(i), '08b') for i in lines)
-    print(binstring,type(binstring))
-    """
-    text = "abcdefgh"
-
+    
     #key = input("Enter 32 Character Key: ")
     key = "12345678123456781234567812345678"                                        #key is string-number
     k = []
     k = make_key(key)                                                               #returns a list of 8 keys each of size 32 bits
-    val = ""
-    for x in text:
-        val = val + format(ord(x),'08b')                                            #convert text to binary string
-    print(val[:32]," ",val[32:])
-    Li,Ri = encrypt_32(val,k)
+
+    Li,Ri = encrypt_32(binstring,k)
 
     val = Li[::-1] + Ri[::-1]
+    
     A = []
     for j in range(0, 64, 8):
         A.append( val[j:j+8] )                                                      #convert to binary chars
     #print(A)
-    for i in range(len(A)):
-        A[i] = chr(int(A[i],2))                                                     #convert to char
+    with open(os.path.join(__location__, "encrypted.txt"),'w') as enc:
+        for i in (A):
+            enc.write(chr(int(i,2)))                                                     #convert to char
+        enc.close()
 
-    print(A)
+    #print(A)
 
     print("would you like to decrypt? 1:Yes 2:Exit")
     choice = input()
     if choice == '1':
-        decrypt(val,k)
+        decrypt(binstring,k)
     else:
         return
